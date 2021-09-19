3d0ba74de3d5bc17098aee32bc2461b07e936251
SydNS <sydneynsiimbe@gmail.com>
2021-09-19 07:40:15 +0300

    this class i for regging the users

---
+++ app/src/main/java/com/example/eric_irene/CustomerRegistration.kt
@@ -0,0 +1,123 @@
+package com.example.eric_irene
+
+import android.content.Intent
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.util.Patterns
+import android.view.View
+import android.widget.Button
+import android.widget.EditText
+import android.widget.TextView
+import android.widget.Toast
+import com.basgeekball.awesomevalidation.AwesomeValidation
+import com.basgeekball.awesomevalidation.ValidationStyle
+import com.google.android.gms.tasks.OnCompleteListener
+import com.google.firebase.auth.AuthResult
+import com.google.firebase.auth.FirebaseAuth
+import com.google.firebase.auth.FirebaseAuth.AuthStateListener
+import com.google.firebase.auth.FirebaseUser
+import java.util.regex.Pattern
+
+class CustomerRegistration : AppCompatActivity() {
+    private lateinit var mEmail: EditText
+    private  lateinit var mPassword:EditText
+    private var mAuth: FirebaseAuth? = null
+    private var firebaseAuthListener: AuthStateListener? = null
+    private lateinit var mLogin: TextView
+    private lateinit var msignup:Button
+
+    private var awesomeValidation: AwesomeValidation? = null
+    private val PASSWORD_PATTERN = Pattern.compile(
+        "^" +
+                "(?=.*[0-9])" +  //at least 1 digit
+                "(?=.*[a-z])" +  //at least 1 lower case letter
+                "(?=.*[A-Z])" +  //at least 1 upper case letter
+                "(?=.*[a-zA-Z])" +  //any letter
+                "(?=.*[@#$%^&+=?])" +  //at least 1 special character
+                "(?=\\S+$)" +  //no white spaces
+                ".{8,}" +  //at least 8 characters
+                "$"
+    )
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_customer_registration)
+        awesomeValidation = AwesomeValidation(ValidationStyle.BASIC)
+        mEmail = findViewById(R.id.email)
+        mPassword = findViewById(R.id.password)
+        mLogin = findViewById(R.id.textView)
+        msignup = findViewById(R.id.signup)
+        awesomeValidation!!.addValidation(
+            this,
+            R.id.email,
+            Patterns.EMAIL_ADDRESS,
+            R.string.error_invalid_email
+        )
+        awesomeValidation!!.addValidation(
+            this,
+            R.id.password,
+            PASSWORD_PATTERN,
+            R.string.error_incorrect_password
+        )
+        mAuth = FirebaseAuth.getInstance()
+        firebaseAuthListener = AuthStateListener {
+            val user = FirebaseAuth.getInstance().currentUser
+            if (user != null) {
+                val intent = Intent(this@CustomerRegistration, CustomerLoginActivity::class.java)
+                startActivity(intent)
+                finish()
+            }
+        }
+        msignup.setOnClickListener(View.OnClickListener {
+            val email = mEmail.getText().toString()
+            val password: String = mPassword.getText().toString()
+            if (awesomeValidation!!.validate()) {
+                mAuth!!.createUserWithEmailAndPassword(email, password).addOnCompleteListener(
+                    this@CustomerRegistration
+                ) { task ->
+                    if (!task.isSuccessful) {
+                        Toast.makeText(
+                            this@CustomerRegistration,
+                            "Sign Up Error",
+                            Toast.LENGTH_SHORT
+                        ).show()
+                    } else {
+                        mAuth!!.currentUser!!.sendEmailVerification()
+                            .addOnCompleteListener { task ->
+                                if (task.isSuccessful) {
+                                    Toast.makeText(
+                                        this@CustomerRegistration,
+                                        "Registered Successfully ! Please, check your Email for verification.",
+                                        Toast.LENGTH_SHORT
+                                    ).show()
+                                } else {
+                                    Toast.makeText(
+                                        this@CustomerRegistration,
+                                        task.exception.message,
+                                        Toast.LENGTH_SHORT
+                                    ).show()
+                                }
+                            }
+                        FirebaseAuth.getInstance().signOut()
+                    }
+                }
+            }
+        })
+        mLogin.setOnClickListener(View.OnClickListener {
+            val intent = Intent(this@CustomerRegistration, CustomerLoginActivity::class.java)
+            startActivity(intent)
+            return@OnClickListener
+        })
+    }
+
+    override fun onStart() {
+        super.onStart()
+        mAuth!!.addAuthStateListener(firebaseAuthListener!!)
+    }
+
+    override fun onStop() {
+        super.onStop()
+        mAuth!!.removeAuthStateListener(firebaseAuthListener!!)
+    }
+
+}
